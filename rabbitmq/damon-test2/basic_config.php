4c3324505e2c5186164d5dd9d33c7b3810ec2118
liuzhengyi <gipsaliu@gmail.com>
2013-05-21 14:16:01 +0800

    add resend function with xmpost

---
+++ rabbitmq/damon-test2/basic_config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * damon-test2/config.php
+ * config of damon-test2
+ * liuzhengyi
+ */
+
+// 系统状态
+define("DEBUG", true);
+//define("DEBUG", false);
+
+// 系统参数
+$conf_curl_timeout = 1;
+
+?>
