7d0df6fdea3016f9c4f44a656f2b3101705fabbc
liuzhengyi <gipsaliu@gmail.com>
2013-06-04 14:35:05 +0800

    deleted some files

--- rabbitmq/damon-test4/docs/sql/resettable_damon2Channels.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists `damon2Channels`;
-create table `damon2Channels` (
-    `channel_id` bigint unsigned auto_increment not null,
-    `creator` bigint unsigned not null, -- create user id
-    `listenner_list` varchar(1024), -- listenner user id, separate by ','. todo: use a single table for listenner_lists
-    `create_time` datetime,
-    `valide` boolean default true, -- 是否有效，为false说明已经失效
-    primary key (`channel_id`)
-) engine=MyISAM auto_increment=1 default charset=utf8;
--- rabbitmq/damon-test4/docs/sql/testdata_damon2Users.sql
+++
@@ -1,4 +0,0 @@
-insert into damon2Users values(NULL, '1234');
-insert into damon2Users values(NULL, '2345');
-insert into damon2Users values(NULL, '3456');
-insert into damon2Users values(NULL, '4567');
